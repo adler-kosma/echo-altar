d333e6736e43766a507de472d2ca25b78f5663f4
adi-pfc <adele.kosman@getpfc.com>
2023-10-14 21:10:26 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import background from './img/saga01.png'
 
 function App() {
   return (
-    <div className="img-background" style={{ backgroundImage: `url(${background})`, height: "1000px", backgroundPosition: "center", backgroundRepeat: "no-repeat"}}>
+    <div className="img-background" style={{ backgroundImage: `url(${background})`, height: "1000px", backgroundPosition: "right", backgroundRepeat: "no-repeat"}}>
       <div className='echo-altar-header'>Echo & Altar Records</div>
     </div>
   );
