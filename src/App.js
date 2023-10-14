9a2d1c03c2eb5091f9310cbb039989882c44afe2
adi-pfc <adele.kosman@getpfc.com>
2023-10-14 21:07:16 +0200

    start from scratch

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import './styles/App.css';
-import Route from './components/Route';
-import Menu from './components/Menu';
-import About from './pages/About';
-import Installations from './pages/Installations';
-import Sounds from './pages/Sounds';
-import Releases from './pages/Releases';
-import background from './img/collage-orgel-shoe.png';
-import Contact from './pages/Contact'
+import './App.css';
+import background from './img/saga01.png'
 
-
-
-export default function App () {
+function App() {
   return (
-      <div className="img-background" style={{ backgroundImage: `url(${background})`, height: "653px"}}>
-        <Menu />
-        <div>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/installations">
-            <Installations />
-          </Route>
-          <Route path="/sounds">
-            <Sounds />
-          </Route>
-          <Route path="/releases">
-            <Releases />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-        </div>
-      </div>
+    <div className="img-background" style={{ backgroundImage: `url(${background})`, height: "1000px", backgroundPosition: "center", backgroundRepeat: "no-repeat"}}>
+      <div className='echo-altar-header'>Echo & Altar Records</div>
+    </div>
   );
 }
+
+export default App;
